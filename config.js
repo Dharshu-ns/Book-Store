664275098c6293f542ccd47f08608c7dd4696da3
dharshu_ns <dharshanans54@gmail.com>
2025-03-26 15:48:40 +0530

    Initial commit

---
+++ config.js
@@ -0,0 +1,2 @@
+export const PORT =4444;
+export const mongoDBURL = 'mongodb+srv://root:229090@book-store-mern.j23oopc.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-MERN'
