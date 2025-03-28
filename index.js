5681cf7d2928d1eb4d6ad3ffc75e7f17bd03d8be
dharshu_ns <dharshanans54@gmail.com>
2025-03-28 20:20:28 +0530

    Formated and update and find one api created

--- index.js
+++ index.js
@@ -7,36 +7,85 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/',(request, response)=>{
-console.log(request);
-return response.status(234).send('Welcome to MERN Stack Tutorial');
-})
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("Welcome to MERN Stack Tutorial");
+});
 
 //Save a new book
-app.post('/books', async (request,response)=>{
-try{
-if(!request.body.title || !request.body.author ||!request.body.publishYear){
-    return response.status(400).send({message: 'Send all required fields: title, author, publishedYear'})
-}
+app.post("/books", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response
+        .status(400)
+        .send({
+          message: "Send all required fields: title, author, publishedYear",
+        });
+    }
 
-const newBook={
-    title: request.body.title,
-    author: request.body.author,
-    publishYear: request.body.publishYear
-}
-const book = await Book.create(newBook)
-return response.status(201).send(book);
-}catch(error){
+    const newBook = {
+      title: request.body.title,
+      author: request.body.author,
+      publishYear: request.body.publishYear,
+    };
+    const book = await Book.create(newBook);
+    return response.status(200).send(book);
+  } catch (error) {
     console.log(error);
-    response.status(500).send({message: error.message});
-}
+    response.status(500).send({ message: error.message });
+  }
 });
 
 // Get all books from the database
-app.get('/allbooks', async(request,response)=>{
+app.get("/books", async (request, response) => {
+  try {
+    const books = await Book.find({});
+    response.status(200).send({
+      count: books.length,
+      data: books,
+    });
+  } catch (error) {
+    console.log(error);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+// Get a book from the database
+app.get("/books/:id", async (request, response) => {
+  try {
+    const {id} = request.params;
+    const book = await Book.findById(id);
+    response.status(200).json(book);
+  } catch (error) {
+    console.log(error);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+//Route for Update an Book
+app.put('/books/:id', async(request, response)=>{
     try{
-const books = await Book.find({});
-response.status(201).send(books);
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+          ) {
+            return response
+              .status(400)
+              .send({
+                message: "Send all required fields: title, author, publishedYear",
+              });
+          }
+          const { id } = request.params; // Fix destructuring of id
+          const result = await Book.findByIdAndUpdate(id, request.body);
+          if(!result){
+            return response.status(404).json({message:'Book not found'})
+          }
+          return response.status(200).send({message:'Book updated successfully'})
     }catch(error){
         console.log(error);
         response.status(500).send({message: error.message});
@@ -45,14 +94,13 @@ response.status(201).send(books);
 
 mongoose
   .connect(mongoDBURL)
-  .then(()=>{
-console.log('App is connected to the database');
+  .then(() => {
+    console.log("App is connected to the database");
 
-app.listen(PORT, ()=>{
-    console.log(`App is running in the port ${PORT}`)
-})
-
+    app.listen(PORT, () => {
+      console.log(`App is running in the port ${PORT}`);
+    });
   })
-  .catch((error)=>{
-    console.log(error)
-  })
+  .catch((error) => {
+    console.log(error);
+  });
